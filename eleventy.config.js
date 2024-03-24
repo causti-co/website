93ed7a0b1589d09a1756969b841874ff9a6b1673
Martín Valdés de León <mvdl@amazon.de>
2024-03-24 12:36:25 +0100

    feat(11ty): static assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,14 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/**");
+  eleventyConfig.addPassthroughCopy("src/assets/icons/**");
+  eleventyConfig.addPassthroughCopy("src/assets/images/**");
+  eleventyConfig.addPassthroughCopy("src/assets/styles/**"); // Eventually needs to be replaced w/ bundling or sass
+  
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist"
     }
   }
 };
