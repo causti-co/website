c6ff66a6189a09cc8ffd81a6d524350cc9f3e9fe
Martín Valdés de León <mvdl@amazon.de>
2024-03-24 18:46:34 +0100

    feat(11ty): photo directory data

--- src/photo/photo.11tydata.js
+++ src/photo/photo.11tydata.js
@@ -1,4 +1,6 @@
 module.exports = {
+  layout: "photo.njk",
+  tags: "photo",
   eleventyComputed: {
     number: data => parseInt(data.page.fileSlug.split("-")[0]),
     imageUrl: data => `/assets/photos/${data.page.fileSlug}.jpg`
