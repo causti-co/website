606b624e3499c234dee1c8ba7b45ede6d3836489
Martín Valdés de León <mvdl@amazon.de>
2024-03-31 23:03:30 +0200

    feat(git): link to github

--- src/_data/commit.js
+++ src/_data/commit.js
@@ -2,11 +2,12 @@ const util = require('node:util');
 const exec = util.promisify(require("node:child_process").exec);
 
 module.exports = async () => {
-  const { stdout } = await exec("git log -1 --format=%h%n%cI%n%s");
-  const [hash, date, subject] = stdout.split("\n");
+  const { stdout } = await exec("git log -1 --format=%H%n%h%n%cI%n%s");
+  const [hash, shortHash, date, subject] = stdout.split("\n");
   
   return {
     hash,
+    shortHash,
     date: new Date(date),
     subject
   };
