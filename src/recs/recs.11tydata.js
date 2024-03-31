ca6cb64a1d1303167390edb4d2d31cfc8e9587b3
Martín Valdés de León <mvdl@amazon.de>
2024-04-01 00:43:49 +0200

    feat(11ty): better recs data

--- src/recs/recs.11tydata.js
+++ src/recs/recs.11tydata.js
@@ -1,8 +1,8 @@
 module.exports = {
-  permalink: false,
-  tags: "recs",
   eleventyComputed: {
     number: data => parseInt(data.page.fileSlug.split("-")[0]),
+    permalink: data => data.page.fileSlug !== "recs" ? false : undefined,
+    tags: data => data.page.fileSlug !== "recs" ? ["recs"] : undefined,
     imageUrl: data => 'exif' in data ? `${data.page.filePathStem}.jpg` : undefined
   }
 };
