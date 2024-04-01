7f7e722dd0a946a765883125a8d4aeaf4139ee82
Martín Valdés de León <mvdl@amazon.de>
2024-04-01 02:22:25 +0200

    feat(11ty): git history for texts

--- src/text/text.11tydata.js
+++ src/text/text.11tydata.js
@@ -1,3 +1,6 @@
+const util = require('node:util');
+const exec = util.promisify(require("node:child_process").exec);
+
 module.exports = {
   layout: "text.njk",
   tags: "text",
@@ -9,6 +12,23 @@ module.exports = {
     permalink: data => {
       if (data.page.filePathStem.includes("/_drafts/")) {
         return `${data.page.filePathStem.replace("/_drafts/", "/")}/`;
+      }
+    },
+    history: async data => {
+      if (data.page.fileSlug !== "text" && "history" in data === false) {
+        const { stdout } = await exec(`git log --format=%H%n%h%n%cI%n%s%n ${data.page.inputPath}`);
+        const history = stdout.trimEnd().split("\n\n").map(logEntry => {
+          const [hash, shortHash, date, subject] = logEntry.split("\n");
+
+          return {
+            hash,
+            shortHash,
+            date: new Date(date),
+            subject
+          };
+        });
+
+        return history;
       }
     }
   }
