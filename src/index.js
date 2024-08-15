04558d8818b63167ff07ce2c2f064cf25b63ed28
Martín Valdés de León <mvdl@amazon.de>
2024-08-15 17:12:02 +0200

    fix: script

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const lucky = Math.random() < 0.2;
 
 if (lucky) {
-  const messages = ["Sowwy, this is a no right-clicking zone", "Sowwy, no right clicking allowed", "No right-clickies! Only lefties!", "Y... you can't just right-click people like that", "Please stop, it hurts", "STOP NOW, PLEASE...PLEASE..."];
+  const messages = ["Sowwy, this is a no right-clicking zone", "Sowwy, no right clicking allowed", "No right-clickies! Only lefties!", "Y... you can't just right-click a website like that", "Please stop, it hurts", "STOP NOW, PLEASE...PLEASE..."];
   const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log('❀');
   window.addEventListener("contextmenu", event => {
-    alert(`${message} (｡>﹏<｡)`);
     event.preventDefault();
+    alert(`${message} (｡>﹏<｡)`);
   });
 }
