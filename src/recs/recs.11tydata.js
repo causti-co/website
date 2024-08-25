fe46dc0e24dbedf1dd6130ad22bd9c23d150b6f2
Martín Valdés de León <mvdl@amazon.de>
2024-08-25 19:03:26 +0200

    feat(recs): setup og data and use it for feeds

--- src/recs/recs.11tydata.js
+++ src/recs/recs.11tydata.js
@@ -3,6 +3,23 @@ module.exports = {
   eleventyComputed: {
     number: data => parseInt(data.page.fileSlug.split("-")[0]),
     permalink: data => data.page.fileSlug !== "recs" ? false : undefined,
+    og: data => {
+      let og = {};
+
+      og.type = "website";
+
+      if (data.imageUrl) {
+        og.image = {
+          url: data.imageUrl,
+          type: "image/jpeg",
+          width: data.exif.width,
+          height: data.exif.height,
+          alt: data.title
+        }
+      }
+
+      return { ...data.og, ...og };
+    },
     imageUrl: data => 'exif' in data ? `/assets/recs/${data.page.fileSlug}.jpg` : undefined
   }
 };
