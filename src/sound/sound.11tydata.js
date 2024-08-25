7a35ceba1d33713d16c91bde979fc04256f414c0
Martín Valdés de León <mvdl@amazon.de>
2024-08-25 15:58:04 +0200

    fix(sound): data title takes precedence over id3 title

--- src/sound/sound.11tydata.js
+++ src/sound/sound.11tydata.js
@@ -4,6 +4,6 @@ module.exports = {
     number: data => parseInt(data.page.fileSlug.split("-")[0]),
     permalink: data => data.page.fileSlug !== "sound" ? false : undefined,
     fileUrl: data => `/assets/sound/${data.page.fileSlug}.mp3`,
-    title: data => 'id3' in data ? data.id3.title : undefined
+    title: data => 'id3' in data && data.title === undefined? data.id3.title : data.title
   }
 };
