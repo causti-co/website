0a9bb9960ea102cbd53fcb7dfce064db3e369c29
Martín Valdés de León <mvdl@amazon.de>
2024-12-30 12:55:08 +0100

    feat(graph): 020, cleanup and help

--- src/graph/020-happy-new-year.code.js
+++ src/graph/020-happy-new-year.code.js
@@ -1,11 +1,11 @@
 // This is the automata definition, in the shape of "From -> To : Condition". The condition receives the first and second neighbor counts.
 // Try fucking with what I did so far.
 
-const a0 = {
+const automata = {
   'Woodland': {
     'Agriculture': (fn, sn) => ((fn.Woodland + sn.Woodland) >= 4 && (fn.Rock + sn.Rock) >= 5) || ((fn.Woodland + sn.Woodland) >= 4 && (fn.Grassland + sn.Grassland) >= 3),
     'Grassland': (fn, sn) => ((fn.Woodland + sn.Woodland) >= 10 && fn.Agriculture == 0) || fn.Grassland >= 5,
-		'Urban': (fn, sn) => (fn.Urban >= 2 && ((fn.Urban + sn.Urban) >= 6))
+    'Urban': (fn, sn) => (fn.Urban >= 2 && ((fn.Urban + sn.Urban) >= 6))
   },
   'Agriculture': {
     'Urban': (fn, sn) => (fn.Urban >= 2 && ((fn.Urban + sn.Urban) >= 5)) || ((fn.Urban + sn.Urban) >= 4 && (fn.Agriculture + sn.Agriculture) >= 4) || ((fn.Urban + sn.Urban) >= 3 && (fn.Grassland + sn.Grassland) >= 6),
@@ -64,15 +64,58 @@ const FRAMES_PER_TICK = FRAME_RATE / SIMULATION_RATE;
 let lastTick = 0;
 let running = true;
 
+class DoubleBuffer { 
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.buffers = [];
+    this.active = 0;
+
+    this.buffers.push(DoubleBuffer.makeBuffer(this.width, this.height));
+    this.buffers.push(DoubleBuffer.makeBuffer(this.width, this.height));
+  }
+
+  activeBuffer() {
+    return this.buffers[this.active];
+  }
+  
+  inactiveBuffer() {
+    return this.buffers[this.inactive()];
+  }
+  
+  inactive() {
+    return this.active == 0 ? 1 : 0;
+  }
+
+  swap() {
+    this.active = this.inactive();
+  }
+
+  copy(src) {
+    let dst = this.activeBuffer();
+
+    for (let j = 0; j < this.height; j++) {
+      for (let i = 0; i < this.width; i++) {
+        dst[j][i] = src[j][i];
+      }
+    }
+  }
+
+  static makeBuffer(width, height) {
+    return Array(height).fill().map(() => Array(width).fill());
+  }
+}
+
+let buffers; // Buffers for automata
 let img; // Your image
 let data = { // Your data
   2023: [],
   1990: []
 };
 
-let buff1, buff2; // Double-buffering
-let active = 1;
-
+let showHelp = false;
+let debug = 0;
+let glyph = 0;
 let selected = undefined;
 
 function distance([r1, g1, b1], [r2, g2, b2]) {
@@ -91,18 +134,6 @@ function findCategory(pixel) {
     }
   }
   return result;
-}
-
-function makeBuffer() {
-  return Array(SIZE_Y).fill().map(() => Array(SIZE_X).fill());
-}
-
-function copyBuffer(dst, src) {
-  for (let j = 0; j < SIZE_Y; j++) {
-    for (let i = 0; i < SIZE_X; i++) {
-      dst[j][i] = src[j][i];
-    }
-  }
 }
 
 function firstNeighbors(buff, x, y) {
@@ -139,16 +170,20 @@ function neighbors(deltas, buff, x, y) {
   }, zero);
 }
 
+function summarize(ns) {
+  return Object.entries(ns).filter(([category, count]) => count > 0).map(([category, count]) => `${category} (${count})`).join(', ');
+}
+
 function nextFrame() {
-  let curr = active == 1? buff1 : buff2;
-  let next = active == 1? buff2 : buff1;
+  let curr = buffers.activeBuffer();
+  let next = buffers.inactiveBuffer();
 
   for (let j = 0; j < SIZE_Y; j++) {
     for (let i = 0; i < SIZE_X; i++) {
       let self = curr[j][i];
       let fn = firstNeighbors(curr, i, j);
       let sn = secondNeighbors(curr, i, j);
-      let rules = a0[self];
+      let rules = automata[self];
       let nextSelf = self;
 
       for (let [candidate, condition] of Object.entries(rules)) {
@@ -162,7 +197,7 @@ function nextFrame() {
     }
   }
 
-  active = active == 1 ? 2 : 1;
+  buffers.swap();
 }
 
 function readImage(i, j, year) {
@@ -216,15 +251,41 @@ p.setup = () => {
     data[1990][SIZE_Y - 1][i] = 'Empty';
   }
 
-  buff1 = makeBuffer();
-  buff2 = makeBuffer();
-  copyBuffer(buff1, data[2023]);
+  buffers = new DoubleBuffer(SIZE_X, SIZE_Y);
+  buffers.copy(data[2023]);
+}
+
+function hexagon() {
+  let c = CELL_SIZE/2;
+  let q = c/HEX_FACTOR;
+
+  p.beginShape();
+  p.vertex(q, 0);
+  p.vertex(q/2, c);
+  p.vertex(-q/2, c);
+  p.vertex(-q, 0);
+  p.vertex(-q/2, -c);
+  p.vertex(q/2, -c);
+  p.endShape(p.CLOSE);
+}
+
+function diamond() {
+  let c = CELL_SIZE/2;
+
+  p.beginShape();
+  p.vertex(c, 0);
+  p.vertex(0, c);
+  p.vertex(-c, 0);
+  p.vertex(0, -c);
+  p.endShape(p.CLOSE);
 }
 
 p.draw = () => {
   p.clear();
+  
+  p.push();
   p.translate((clientWidth - CELL_SIZE * SIZE_X) / 2, 0);
-  let d = active == 1 ? buff1 : buff2;
+  let buff = buffers.activeBuffer();
   let d1a = [[-1, -1], [0, -1], [+1, -1], [-1, 0], [0, +1], [+1, 0]];
   let d2a = [[-1, -2], [+1, -2], [-2, 0], [+2, 0], [-1, +1], [+1, +1]];
   let d1b = [[-1, 0], [0, -1], [+1, 0], [-1, +1], [0, +1], [+1, +1]];
@@ -234,51 +295,86 @@ p.draw = () => {
     for (let i = 0; i < SIZE_X; i++) {
       let x = i * CELL_SIZE * HEX_FACTOR;
       let y = j * CELL_SIZE + CELL_SIZE / 4 * (i % 2 ? -1 : 1);
-      let [r, g, b] = CATEGORIES[d[j][i]];
+      let [r, g, b] = CATEGORIES[buff[j][i]];
 
       p.fill(r, g, b);
+      p.stroke(r, g, b);
       
-      if (selected) {
-        let d1 = i % 2 ? d1b : d1a;
-        let d2 = i % 2 ? d2b : d2a;
-        if (i == selected.x && j == selected.y) {
-          p.fill('red');  
-        }
-        for (let [dx, dy] of d1) {
-          if (i == selected.x + dx && j == selected.y + dy) {
-            p.fill('yellow');  
-          }
-        }
-        for (let [dx, dy] of d2) {
-          if (i == selected.x + dx && j == selected.y + dy) {
-            p.fill('magenta');  
-          }
-        }
+      switch (debug) {
+        case 1:
+          if (i % 2) p.fill('hotpink');
+          break;
+        case 2:
+          if (j % 2) p.fill('hotpink');
+          break;
+        case 3:
+          if (selected) {
+            let d1 = i % 2 ? d1b : d1a;
+            let d2 = i % 2 ? d2b : d2a;
+            if (i == selected.i && j == selected.j) {
+              p.fill('red');  
+            }
+            for (let [di, dj] of d1) {
+              if (i == selected.i + di && j == selected.j + dj) {
+                p.fill('yellow');  
+              }
+            }
+            for (let [di, dj] of d2) {
+              if (i == selected.i + di && j == selected.j + dj) {
+                p.fill('magenta');  
+              }
+            }
+          }    
+          break;
       }
-
-      //if (i % 2) p.fill('darkorange');
 
       p.push();
       p.translate(x, y);
-
-      p.strokeWeight(0.5);
-      p.stroke(100);
-      let c = CELL_SIZE/2;
-      let q = c / HEX_FACTOR;
-
-      p.beginShape();
-      p.vertex(q, 0);
-      p.vertex(q/2, c);
-      p.vertex(-q/2, c);
-      p.vertex(-q, 0);
-      p.vertex(-q/2, -c);
-      p.vertex(q/2, -c);
-      p.endShape(p.CLOSE);
-
-      // noStroke();
-      // circle(0, 0, CELL_SIZE);
+      switch (glyph) {
+        case 0:
+          p.strokeWeight(0.5);
+          p.stroke(100);
+          hexagon();
+          break;
+        case 1:
+          p.noStroke();
+          p.circle(0, 0, CELL_SIZE);
+          break;
+        case 2:
+          p.strokeWeight(2);
+          diamond();
+          break;
+      }
       p.pop();
     }
+  }
+  p.pop();
+
+  p.stroke('black');
+  p.strokeWeight(4.0);
+  p.fill('hotpink');
+  p.textSize(16);
+  p.textStyle(p.BOLD);
+
+  if (debug == 3 && selected) {
+    let {i, j} = selected;
+    let fn = firstNeighbors(buff, i, j);
+    let sn = secondNeighbors(buff, i, j);
+
+    p.text(`(${i}, ${j}): ${buff[j][i]}`, 8, 16 * 1);
+    p.text(`1st Neighbors: ${summarize(fn)}`, 8, 16 * 2);
+    p.text(`2nd Neighbors: ${summarize(sn)}`, 8, 16 * 3);
+  }
+
+  if (showHelp) {
+    p.text(`Press 1 to reset w/ 2023 data.`, 8, p.height - 16 * 5);
+    p.text(`Press 2 to reset w/ 1990 data.`, 8, p.height - 16 * 4);
+    p.text(`Press P to toggle automata.`, 8, p.height - 16 * 3);
+    p.text(`Press G to cycle glyphs.`, 8, p.height - 16 * 2);
+    p.text(`Press D to cycle debugging modes.`, 8, p.height - 16 * 2);
+    p.text(`Press ? to go hide.`, 8, p.height - 16 * 1);
+  } else {
+    p.text(`Press ? to toggle instructions.`, 16, p.height - 16);
   }
   
   if (running && p.frameCount - lastTick >= FRAMES_PER_TICK) {
@@ -287,19 +383,39 @@ p.draw = () => {
   }
 }
 
-// p.mousePressed = () => {
-//   let x = Math.ceil(p.mouseX / (CELL_SIZE * HEX_FACTOR));
-//   let y = Math.ceil(p.mouseY / CELL_SIZE);
-  
-//   // selected = {x, y};
-//   // console.log(x, y);
-  
-//   // let d = active == 1 ? buff1 : buff2;
-//   // console.log(firstNeighbors(d, x, y));
-//   // console.log(secondNeighbors(d, x, y));
-//   // nextFrame();
-// }
-
-p.doubleClicked = () => {
-  running = !running;
+p.keyPressed = () => {
+  switch (p.key) {
+    case '1':
+      buffers.copy(data[2023]);
+      break;
+    case '2':
+      buffers.copy(data[1990]);
+      break;
+    case 'p':
+    case 'P':
+      running = !running;
+      break;
+    case 'g':
+    case 'G':
+      glyph = (glyph + 1) % 3;
+      break;
+    case 'd':
+    case 'D':
+      debug = (debug + 1) % 4;
+      break;
+    case '?':
+      showHelp = !showHelp;
+      break;
+  }
 }
+
+p.mousePressed = () => {
+  let offsetX = (clientWidth - CELL_SIZE * SIZE_X) / 2;
+
+  let i = Math.ceil((p.mouseX - offsetX) / (CELL_SIZE * HEX_FACTOR));
+  let j = Math.ceil(p.mouseY / CELL_SIZE);
+  
+  selected = {i, j};
+}
+
+// shutup
